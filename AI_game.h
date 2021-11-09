7af89b1f4911ee32f64f6bdaef60a67ef4cb4b96
TrueDemonDay <82235675+TrueDemonDay@users.noreply.github.com>
2021-11-10 02:10:11 +0300

    Create AI_game.h

---
+++ AI_game.h
@@ -0,0 +1,12 @@
+#pragma once
+
+void DumbTurn(int A[]);
+void EnemyTurn(int A[], int b);
+bool CheckVictory(int A[]);
+bool IfItDraw(int A[]);
+void PrintTable(int A[]);
+void ClearTable(int A[]);
+void SmartTurn(int A[], int g);
+int ChooseDiff();
+void ThinkTurn(int A[]);
+void RemeberGame(int A[]);
